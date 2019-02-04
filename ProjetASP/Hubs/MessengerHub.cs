f8883d84e011a24072fcccdd8fb00fa82263d7f7
Alexis Delhaie <roboform@hotmail.fr>
2019-02-04 18:36:59 +0100

    autre serveur de sauvegarde

--- ProjetASP/Hubs/MessengerHub.cs
+++ ProjetASP/Hubs/MessengerHub.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace ProjetASP.Hubs
@@ -11,6 +10,14 @@ namespace ProjetASP.Hubs
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+            using (WebClient wc = new WebClient())
+            {
+                var reqparm = new System.Collections.Specialized.NameValueCollection();
+                reqparm.Add("user", Uri.EscapeDataString(user));
+                reqparm.Add("message", Uri.EscapeDataString(message));
+                wc.UploadValuesAsync(new Uri("http://192.168.43.211:3000/back"), "POST", reqparm);
+            }
         }
     }
 }
--- ProjetASP/wwwroot/js/Messenger.js
+++ ProjetASP/wwwroot/js/Messenger.js
@@ -11,8 +11,11 @@ connection.on('ReceiveMessage', function (user, message) {
 	var node = document.createElement("li");
 	var nodeText = document.createTextNode(user + ": " + message);
 	node.className = "list-group-item";
-	node.appendChild(nodeText);
-	messageList.appendChild(node);
+    node.appendChild(nodeText);
+    if (messageList.childElementCount == 0)
+        messageList.appendChild(node);
+    else
+        messageList.insertBefore(node, messageList.firstChild);
 });
 
 connection.start().catch(function (err) {
@@ -21,9 +24,13 @@ connection.start().catch(function (err) {
 
 document.getElementById('messageSubmit').addEventListener('click', function (event) {
     event.preventDefault();
-    //var user = document.getElementById('user').value;
-	var user = "blabla";
+    var user = document.getElementById('userInput').value;
     var message = document.getElementById('messageInput').value;
+
+    if (user == "")
+        return;
+    if (message == "")
+        return;
 
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
